bdc282875b15fee0be1ee56026fd1923f08fffbb
brianchennn <ny40ny40ny.cs11@nycu.edu.tw>
2023-05-05 18:56:19 +0000

    implement RESTful API: Get method

--- server.go
+++ server.go
@@ -8,10 +8,21 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	router.LoadHTMLGlob("template/*")
+	router.Static("/asset", "./asset")
+	router.GET("/ping", ping)
+	router.GET("/RecordPage", getRecordPage)
+
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+}
+
+func ping(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
+
+func getRecordPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", nil)
 }
